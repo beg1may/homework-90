20b25a5b1f9f447ebc7d1c20292f52b8f32de9c4
Silalieva Begimay <begimayomurovna@gmail.com>
2025-05-13 11:30:59 +0600

    Реализован WebSocket для приёма пикселей всем клиентам

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import expressWs from "express-ws";
 import cors from "cors";
+import {WebSocket} from "ws";
 
 const app = express();
 const wsInstance = expressWs(app);
@@ -11,11 +12,43 @@ app.use(cors());
 const router = express.Router();
 wsInstance.applyTo(router);
 
-router.ws('/chat', (ws, req) => {
-    console.log('Client connected');
+const connectedClient: WebSocket[] = [];
+interface IncomingPixels {
+    x: number;
+    y: number;
+}
+
+const pixels: IncomingPixels[] = [];
+
+router.ws('/pixel', (ws, req) => {
+    connectedClient.push(ws);
+
+    ws.send(JSON.stringify({
+        type: "SEND_PIXEL",
+        pixels: pixels
+    }));
+
+    ws.on('pixel', (pixel) => {
+        try {
+            const decodedPixel = JSON.parse(pixel.toString());
+
+            if (decodedPixel.type === "NEW_PIXEL") {
+                pixels.push(decodedPixel.payload);
+                connectedClient.forEach(clientWS => {
+                    clientWS.send(JSON.stringify({
+                        type: "NEW_PIXEL",
+                        payload: decodedPixel.payload
+                    }));
+                });
+            }
+        } catch (e) {
+            ws.send(JSON.stringify({error: e }));
+        }
+    });
 
     ws.on('close', () => {
-        console.log("Client disconnectedимп");
+        const index = connectedClient.indexOf(ws);
+        connectedClient.splice(index, 1);
     })
 });
 
